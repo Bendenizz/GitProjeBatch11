22675b1a5c554a3dcb5adf9eca652015b62b49ad
Bendenizz <74242175+Bendenizz@users.noreply.github.com>
2020-11-19 11:09:53 +0300

    Update BirinciGun.java

--- src/BirinciGun.java
+++ src/BirinciGun.java
@@ -3,6 +3,7 @@ public class BirinciGun {
 	public static void main(String[] args) {
 		System.out.println("11.57 - Birinci Gün");
 		System.out.println("İkinci Commit");
+		System.out.println("10:11 - İkinci gün");
 		
 	}
 }
