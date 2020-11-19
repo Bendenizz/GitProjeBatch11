fdbe3cd2248f7689831256ca72ef258c11c2123a
Bendenizz <atesdeniz119@gmail.com>
2020-11-19 11:27:28 +0300

    İkinci gün ikinci commit

--- src/BirinciGun.java
+++ src/BirinciGun.java
@@ -3,7 +3,8 @@ public class BirinciGun {
 	public static void main(String[] args) {
 		System.out.println("11.57 - Birinci Gün");
 		System.out.println("İkinci Commit");
-		System.out.println("10:11 - İkinci gün");
+		System.out.println("10:11 - İkinci gün"); 
+		System.out.println("10:26 - İkinci gün"); 
 		
 	}
 }
